a49b8e733e21e8b7e2102dbc8f6e2affe0d7088a
nextbest <skalkowskimateusz@gmial.com>
2020-01-12 14:29:52 +0100

    create network service

---
+++ app/src/main/java/com/nextbest/weatherappandroid/data/network/NetworkService.kt
@@ -0,0 +1,31 @@
+package com.nextbest.weatherappandroid.data.network
+
+import com.nextbest.weatherappandroid.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+class NetworkService {
+
+    private val retrofit: Retrofit
+
+    init {
+        retrofit = Retrofit.Builder()
+            .client(createHttpClient())
+            .baseUrl(BuildConfig.API_PATH)
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
+    }
+
+    private fun createHttpClient(): OkHttpClient {
+        val loggingInterceptor = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+        return OkHttpClient.Builder().addInterceptor(loggingInterceptor).build()
+    }
+
+    fun getRetorfit() = retrofit
+}
