5a7a0ee9cea267af19c14024e360d9e0c703f481
nextbest <skalkowskimateusz@gmial.com>
2020-01-12 14:45:23 +0100

    add api class

--- app/src/main/java/com/nextbest/weatherappandroid/di/ApplicationModule.kt
+++ app/src/main/java/com/nextbest/weatherappandroid/di/ApplicationModule.kt
@@ -2,6 +2,7 @@ package com.nextbest.weatherappandroid.di
 
 import android.app.Application
 import android.content.Context
+import com.nextbest.weatherappandroid.data.network.NetworkService
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
@@ -12,4 +13,8 @@ class ApplicationModule {
     @Provides
     @Singleton
     fun provideContext(application: Application): Context = application
+
+    @Provides
+    @Singleton
+    fun provideNetworkService(): NetworkService = NetworkService()
 }
