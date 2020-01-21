40baea0df9c0a052a1175dd4c2bcd90f52836b32
nextbest <skalkowskimateusz@gmial.com>
2020-01-21 20:39:10 +0100

    create splash activity and set it as first screen

---
+++ app/src/main/java/com/nextbest/weatherappandroid/screen/splash/SplashActivity.kt
@@ -0,0 +1,11 @@
+package com.nextbest.weatherappandroid.screen.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
