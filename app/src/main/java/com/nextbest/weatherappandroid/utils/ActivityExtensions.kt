fe6118437ec3754bdc97634b2bdf5ac999a37010
nextbest <skalkowskimateusz@gmial.com>
2020-01-22 22:12:45 +0100

    create view for main activity

--- app/src/main/java/com/nextbest/weatherappandroid/utils/ActivityExtensions.kt
+++ app/src/main/java/com/nextbest/weatherappandroid/utils/ActivityExtensions.kt
@@ -1,8 +1,15 @@
 package com.nextbest.weatherappandroid.utils
 
-import android.app.Activity
 import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.nextbest.weatherappandroid.R
 
-fun <T> Activity.openActivity(it: Class<T>) {
+fun <T> AppCompatActivity.openActivity(it: Class<T>) {
     startActivity(Intent(this, it))
+}
+
+fun AppCompatActivity.pushFragment(fragment: Fragment) {
+    supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, fragment)
+        .commit()
 }
