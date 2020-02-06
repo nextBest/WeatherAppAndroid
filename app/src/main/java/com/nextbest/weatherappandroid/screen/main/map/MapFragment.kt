4b88025194d6954811c31e87965074b0ea14332d
nextbest <skalkowskimateusz@gmial.com>
2020-02-06 21:04:34 +0100

    implement map screen

--- app/src/main/java/com/nextbest/weatherappandroid/screen/main/map/MapFragment.kt
+++ app/src/main/java/com/nextbest/weatherappandroid/screen/main/map/MapFragment.kt
@@ -1,19 +1,32 @@
 package com.nextbest.weatherappandroid.screen.main.map
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.snackbar.Snackbar
 import com.nextbest.weatherappandroid.R
 import com.nextbest.weatherappandroid.screen.BaseViewModelFragment
+import com.nextbest.weatherappandroid.utils.*
+import kotlinx.android.synthetic.main.fragment_map.*
 
 
 class MapFragment : BaseViewModelFragment<MapViewModel>(), OnMapReadyCallback {
 
     override fun getViewModelClass() = MapViewModel::class.java
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     private var googleMap: GoogleMap? = null
 
@@ -21,6 +34,9 @@ class MapFragment : BaseViewModelFragment<MapViewModel>(), OnMapReadyCallback {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        context?.let {
+            fusedLocationClient = LocationServices.getFusedLocationProviderClient(it)
+        }
         return inflater.inflate(R.layout.fragment_map, container, false)
     }
 
@@ -31,9 +47,110 @@ class MapFragment : BaseViewModelFragment<MapViewModel>(), OnMapReadyCallback {
                 this
             )
         }
+        checkLocationPermission()
     }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel.showMarkerOnMap.observe(this) {
+            showMarkerOnMap(it.first, it.second)
+        }
+        viewModel.setCameraPosition.observe(this) {
+            setCameraPosition(it.first, it.second)
+        }
+        viewModel.showLoader.observe(this) {
+            loaderView.setVisibility(it)
+        }
+        viewModel.showErrorMessageSnackBar.observeEvent(this) {
+            showErrorSnackBar(it)
+        }
+        viewModel.goToWeatherDetailsScreen.observeEvent(this) {
+            // TODO go to weather details screen
+        }
+    }
+
+    private fun checkLocationPermission() {
+        if (context?.checkPermissionGranted(Manifest.permission.ACCESS_COARSE_LOCATION) == true) {
+            searchUserLocation()
+        } else {
+            activity?.let {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        it,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    )
+                ) {
+                    showPermissionRationaleDialog()
+                } else {
+                    requestLocationPermission()
+                }
+            }
+        }
+    }
+
+    private fun showMarkerOnMap(latitude: Double, longitude: Double) {
+        val markerPosition = LatLng(latitude, longitude)
+        googleMap?.let {
+            it.clear()
+            it.addMarker(MarkerOptions().position(markerPosition))
+        }
+    }
+
+    private fun setCameraPosition(latitude: Double, longitude: Double) {
+        googleMap?.animateCamera(CameraUpdateFactory.newLatLngZoom(LatLng(latitude, longitude), 6f))
+    }
+
+    private fun requestLocationPermission() {
+        activity?.let {
+            ActivityCompat.requestPermissions(
+                it,
+                arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
+                LOCATION_PERMISSION_REQUEST_CODE
+            )
+        }
+    }
+
+    private fun showPermissionRationaleDialog() {
+        context?.let {
+            AlertDialog.Builder(it)
+                .setTitle(R.string.rational_dialog_title)
+                .setMessage(R.string.rational_dialog_message)
+                .setPositiveButton(
+                    R.string.rational_dialog_accept_button
+                ) { _, _ ->
+                    requestLocationPermission()
+                }
+                .setNegativeButton(R.string.rational_dialog_reject_button, null)
+                .show()
+        }
+    }
+
+    private fun searchUserLocation() {
+        fusedLocationClient.lastLocation.addOnSuccessListener {
+            viewModel.locationFind(it.latitude, it.longitude)
+        }.addOnFailureListener {
+            it.printStackTrace()
+        }
+    }
+
 
     override fun onMapReady(googleMap: GoogleMap?) {
         this.googleMap = googleMap
+        googleMap?.setOnMapClickListener {
+            viewModel.tapOnMap(it.latitude, it.longitude)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            searchUserLocation()
+        }
+    }
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
     }
 }
--- app/src/main/java/com/nextbest/weatherappandroid/screen/main/map/MapViewModel.kt
+++ app/src/main/java/com/nextbest/weatherappandroid/screen/main/map/MapViewModel.kt
@@ -1,8 +1,11 @@
 package com.nextbest.weatherappandroid.screen.main.map
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.nextbest.weatherappandroid.data.model.WeatherData
 import com.nextbest.weatherappandroid.data.repository.WeatherRepository
 import com.nextbest.weatherappandroid.screen.BaseViewModel
+import com.nextbest.weatherappandroid.utils.Event
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
@@ -14,9 +17,34 @@ class MapViewModel @Inject constructor(private val weatherRepository: WeatherRep
 
     private var searchLocationByCoordinatesSingle: Single<WeatherData>? = null
 
+    private val _showMarkerOnMap = MutableLiveData<Pair<Double, Double>>()
+    val showMarkerOnMap: LiveData<Pair<Double, Double>>
+        get() = _showMarkerOnMap
+
+    private val _setCameraPosition = MutableLiveData<Pair<Double, Double>>()
+    val setCameraPosition: LiveData<Pair<Double, Double>>
+        get() = _setCameraPosition
+
+    private val _showLoader = MutableLiveData<Boolean>().apply { value = false }
+    val showLoader: LiveData<Boolean>
+        get() = _showLoader
+
+    private val _goToWeatherDetailsScreen = MutableLiveData<Event<WeatherData>>()
+    val goToWeatherDetailsScreen: LiveData<Event<WeatherData>>
+        get() = _goToWeatherDetailsScreen
+
+    private val _showErrorMessageSnackBar = MutableLiveData<Event<String>>()
+    val showErrorMessageSnackBar: LiveData<Event<String>>
+        get() = _showErrorMessageSnackBar
+
+
     fun tapOnMap(latitude: Double, longitude: Double) {
-        // TODO set position on map
-        searchLocationByCoordinates(longitude, longitude)
+        _showMarkerOnMap.value = Pair(latitude, longitude)
+        searchLocationByCoordinates(latitude, longitude)
+    }
+
+    fun locationFind(latitude: Double, longitude: Double) {
+        _setCameraPosition.value = Pair(latitude, longitude)
     }
 
     private fun searchLocationByCoordinates(latitude: Double, longitude: Double) {
@@ -35,13 +63,18 @@ class MapViewModel @Inject constructor(private val weatherRepository: WeatherRep
         return searchLocationByCoordinatesSingle?.let {
             it.subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
+                .doOnSubscribe {
+                    _showLoader.value = true
+                }
                 .doAfterTerminate {
+                    _showLoader.value = false
                     searchLocationByCoordinatesSingle = null
                 }
-                .subscribe({
-
-                }, {
-
+                .subscribe({ weatherData ->
+                    _goToWeatherDetailsScreen.value = Event(weatherData)
+                }, { error ->
+                    // TODO get message
+                    _showErrorMessageSnackBar.value = Event(error.localizedMessage)
                 })
         }
     }
