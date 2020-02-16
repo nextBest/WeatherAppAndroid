cd313501ff3a98842549feb49e6643cf38513e4d
nextbest <skalkowskimateusz@gmial.com>
2020-02-16 19:18:04 +0100

    create weather details activity

---
+++ app/src/main/java/com/nextbest/weatherappandroid/screen/weatherdetails/WeatherDetails.kt
@@ -0,0 +1,13 @@
+package com.nextbest.weatherappandroid.screen.weatherdetails
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.nextbest.weatherappandroid.R
+
+class WeatherDetails : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weather_details)
+    }
+}
