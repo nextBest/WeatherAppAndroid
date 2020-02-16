cb94924b10e1230d52bd925145d4bf259e8512a0
nextbest <skalkowskimateusz@gmial.com>
2020-02-16 19:50:45 +0100

    add push arguments to details activity

--- app/src/main/java/com/nextbest/weatherappandroid/data/model/Location.kt
+++ app/src/main/java/com/nextbest/weatherappandroid/data/model/Location.kt
@@ -1,9 +1,11 @@
 package com.nextbest.weatherappandroid.data.model
 
+import java.io.Serializable
+
 data class Location(
     val distance: Int?,
     val latt_long: String,
     val location_type: String,
     val title: String,
     val woeid: Int
-)
+): Serializable
--- app/src/main/java/com/nextbest/weatherappandroid/screen/weatherdetails/WeatherDetailsActivity.kt
+++ app/src/main/java/com/nextbest/weatherappandroid/screen/weatherdetails/WeatherDetailsActivity.kt
@@ -1,13 +1,36 @@
 package com.nextbest.weatherappandroid.screen.weatherdetails
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import com.nextbest.weatherappandroid.R
+import com.nextbest.weatherappandroid.data.model.Location
+import com.nextbest.weatherappandroid.data.model.WeatherData
+import com.nextbest.weatherappandroid.screen.weatherdetails.actualweather.ActualWeatherFragment
+import com.nextbest.weatherappandroid.utils.pushFragment
+import dagger.android.support.DaggerAppCompatActivity
 
-class WeatherDetailsActivity : AppCompatActivity() {
+class WeatherDetailsActivity : DaggerAppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_weather_details)
+        if (savedInstanceState == null) {
+            pushFragment(ActualWeatherFragment())
+        }
+    }
+
+    companion object {
+        private const val WEATHER_DATA = "WEATHER_DATA"
+        private const val LOCATION = "LOCATION"
+
+        fun newIntent(
+            context: Context,
+            weatherData: WeatherData? = null,
+            location: Location? = null
+        ) = Intent(context, WeatherDetailsActivity::class.java).apply {
+            putExtra(WEATHER_DATA, weatherData)
+            putExtra(LOCATION, location)
+        }
     }
 }
