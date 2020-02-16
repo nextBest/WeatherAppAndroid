3ab799aa431c546fff0db869d7451448cf4c9cbf
nextbest <skalkowskimateusz@gmial.com>
2020-02-16 12:21:47 +0100

    add sort list by string

--- app/src/main/java/com/nextbest/weatherappandroid/data/repository/WeatherRepositoryImpl.kt
+++ app/src/main/java/com/nextbest/weatherappandroid/data/repository/WeatherRepositoryImpl.kt
@@ -22,6 +22,9 @@ class WeatherRepositoryImpl(private val weatherApi: WeatherApi) : WeatherReposit
 
     override fun getWeatherInfo(woeid: Int) = weatherApi.getWeatherDetails(woeid.toString())
 
-    override fun searchCity(query: String) = weatherApi.getLocationByQuery(query)
+    override fun searchCity(query: String) =
+        weatherApi.getLocationByQuery(query).map { locationList ->
+            locationList.sortedBy { it.title }
+        }
 
 }
