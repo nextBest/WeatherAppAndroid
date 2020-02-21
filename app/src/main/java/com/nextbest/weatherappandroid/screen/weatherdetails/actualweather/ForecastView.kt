65dbb531c9e1c365f8b1bd9e878fcf7caa61c86d
nextbest <skalkowskimateusz@gmial.com>
2020-02-21 21:50:20 +0100

    create forecast view

---
+++ app/src/main/java/com/nextbest/weatherappandroid/screen/weatherdetails/actualweather/ForecastView.kt
@@ -0,0 +1,34 @@
+package com.nextbest.weatherappandroid.screen.weatherdetails.actualweather
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.FrameLayout
+import com.bumptech.glide.Glide
+import com.nextbest.weatherappandroid.BuildConfig
+import com.nextbest.weatherappandroid.R
+import com.nextbest.weatherappandroid.data.model.Weather
+import com.nextbest.weatherappandroid.utils.temperature
+import kotlinx.android.synthetic.main.forecast_view.view.*
+
+class ForecastView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : FrameLayout(context, attrs, defStyleAttr) {
+
+    init {
+        initView()
+    }
+
+    private fun initView() {
+        View.inflate(context, R.layout.forecast_view, this)
+    }
+
+    fun setupView(weather: Weather) {
+        day.text = weather.applicable_date.toString()
+        Glide.with(context).load(BuildConfig.IMAGE_PATH + weather.weather_state_abbr + ".png")
+            .into(weatherImage)
+        minTemp.text = weather.min_temp.temperature()
+        maxTemp.text = weather.max_temp.temperature()
+    }
+
+}
